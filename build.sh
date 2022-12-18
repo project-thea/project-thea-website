80a33e9ab1e68061263471237162dce3f016e233
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2022-12-18 15:59:37 +0300

    Update website

---
+++ build.sh
@@ -0,0 +1,19 @@
+yarn run build #
+
+#cp ... dit/
+#
+cp privacy-policy.html dist/
+mkdir -p dist/assets 
+cp src/assets/* dist/assets/
+git add ./*
+git commit -m "Update website" -n
+git checkout master # you can avoid this line if you are in master...
+git subtree split --prefix dist -b gh-pages # create a local gh-pages branch containing the splitted output folder
+git push -f origin gh-pages:gh-pages # force the push of the gh-pages branch to the remote gh-pages branch at origin
+git branch -D gh-pages # delete the local gh-pages because you will need it: ref
+
+
+#https://raw.githubusercontent.com/project-thea/project-thea-website/master/src/assets/THEA-DCT-Poster.pdf
+#CNAME https://project-thea.org/
+
+
--- src/Resources/ProgresTimeline.jsx
+++ src/Resources/ProgresTimeline.jsx
@@ -5,9 +5,21 @@ import OverPack from 'rc-scroll-anim/lib/ScrollOverPack';
 import { getChildrenToRender, isImg } from './utils';
 import { ClockCircleOutlined, FilePdfOutlined } from '@ant-design/icons';
 
-const data = [
+const resourceList = [
   {
-    title: 'Ant Design Title 1',
+    title: 'Driver Sensitisation Information',
+	url: 'https://github.com/project-thea/resources/raw/main/Driver%20Information.pdf',
+	desc: "This contains sensitisation information for the drivers. It\'s provided with translated versions in Luganda and Kiswahili"
+  },
+  {
+    title: 'THEA-DCT Poster',
+	url: 'https://raw.githubusercontent.com/project-thea/project-thea-website/master/src/assets/THEA-DCT-Poster.pdf',
+	desc: 'THEA-DCT poster used during the 16th December 2022 handover ceremony of the project to the Ministry of Health'
+  },
+  {
+    title: 'THEA-DCT Technology presentation',
+	url: 'https://raw.githubusercontent.com/project-thea/project-thea-website/master/src/assets/THEA-DCT-Technology-Presentation.pdf',
+	desc: 'THEA-DCT technology presentation used during the 16th December 2022 handover ceremony of the project to the Ministry of Health'
   },
 ];
 
@@ -15,19 +27,20 @@ class ProgresTimeline extends React.PureComponent {
   getBlockChildren = (data) =>
     data.map((item, i) => {
       const { titleWrapper, image, ...$item } = item;
+	  console.log("item:", item);
       return (
         <Col key={i.toString()}>
           <Row>
             <Col span={24}>
 			<List
 				itemLayout="horizontal"
-				dataSource={data}
+				dataSource={resourceList}
 				renderItem={item => (
 				  <List.Item>
 					<List.Item.Meta
 					  avatar={<FilePdfOutlined />}
-					  title={<a href="https://github.com/project-thea/resources/raw/main/Driver%20Information.pdf"> <FilePdfOutlined /> Driver Sensitisation Information</a>}
-					  description="This contains sensitisation information for the drivers. It's provided with translated versions in Luganda and Kiswahili"
+					  title={<a href={item.url}> <FilePdfOutlined /> {item.title}</a>}
+					  description={item.desc}
 					/>
 				  </List.Item>
 				)}
